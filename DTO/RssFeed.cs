7b0d2545e17008d679ed0a8b8386c9e3979bf5e6
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-11-13 14:42:36 +0100

    add DTO project and RssFeed, RssItem DTO's

---
+++ DTO/RssFeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class RssFeed
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> Categories { get; set; }
+        public List<RssItem> Items { get; set; }
+    }
+}
---
+++ DTO/RssItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    internal class RssItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string Duration { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
