0a5c3264a0a05b6f39011c0667c7ee9c95983423
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-11-19 10:43:28 +0100

    Add podcast Page and change theme to spotify like

---
+++ UI/MVVM/ViewModel/PodcastViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Core;
+using UI.MVVM.Model;
+
+namespace UI.MVVM.ViewModel
+{
+    class PodcastViewModel : ObservableObject
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+        public ObservableCollection<Episode> Episodes { get; set; } = new();
+
+        public PodcastViewModel()
+        {
+            Title = "Test Podcast";
+            for (int i = 10; i > 0; i--)
+            {
+                Episodes.Add(new Episode
+                {
+                    Title = "#" + i + "-" + "Episode",
+                    Description = "Test description. Here is text that text is text which can be text.",
+                    EpisodeNumber = i,
+                });
+            }
+        }
+    }
+}
