2cc07b6e0cede260d588d94b97c1b0d29d97753a
Amahaag <amadeus_00@live.se>
2025-11-19 14:56:12 +0100

    Repository för Category och Subscription
    Fixat ett repository för både Category och Subscription.

---
+++ DAL/MongoDB/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using DAL.MongoDB.Interfaces;
+using Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.MongoDB
+{
+    public class CategoryRepository : MongoRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IMongoDatabase db)
+            : base(db, "Categories") { }
+
+        public async override Task<Category> AddAsync(Category entity)
+        {
+            try
+            {
+                await _collection.InsertOneAsync(entity);
+                return entity;
+            }
+            catch (MongoWriteException ex)
+                when (ex.WriteError.Category == ServerErrorCategory.DuplicateKey)
+            {
+                return await _collection.Find(e => e.Name == entity.Name).FirstOrDefaultAsync();
+            }
+        }
+    }
+}
---
+++ DAL/MongoDB/SubscriptionRepository.cs
@@ -0,0 +1,51 @@
+﻿using DAL.MongoDB.Interfaces;
+using Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.MongoDB
+{
+    public class SubscriptionRepository : MongoRepository<Subscription>, ISubscriptionRepository
+    {
+        public SubscriptionRepository(IMongoDatabase db)
+            : base(db, "Subscriptions") 
+        {
+            var keys = Builders<Subscription>.IndexKeys
+                   .Ascending(s => s.UserId)
+                   .Ascending(s => s.PodcastId);
+
+            var options = new CreateIndexOptions
+            {
+                Unique = true
+            };
+
+            _collection.Indexes.CreateOne(
+                new CreateIndexModel<Subscription>(keys, options)
+            );
+
+        }
+
+
+        public async override Task<Subscription> AddAsync(Subscription entity)
+        {
+            try
+            {
+                await _collection.InsertOneAsync(entity);
+                return entity;
+            }
+            catch (MongoWriteException ex)
+               when (ex.WriteError != null &&
+                    (ex.WriteError.Category == ServerErrorCategory.DuplicateKey ||
+                     ex.WriteError.Code == 11000))
+            {
+                return await _collection.Find(s => s.UserId == entity.UserId && s.PodcastId == entity.PodcastId)
+                .FirstOrDefaultAsync();
+            }
+        }
+    }
+}
+
